3ad017635297437e821e703877ce9819563df9ba
shahzad2003 <shahzad.2023012005@tulas.edu.in>
2024-11-01 21:12:45 +0530

    shady 001

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+// Components
+import MyNavbar from "./components/Nav";
+import Categories from "./components/Categories";
+import Questions from "./components/Question";
+
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+      <Routes>
+        <Route path="/" element={<Categories />} />{" "}
+        {/* Render Categories on Home */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+       
+         {/* Dynamic category route */}
+        <Route path="/:category" element={<Questions/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+// src/components/Navbar.js
+
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/Nav.css"; // Custom CSS for additional styling
+
+const MyNavbar = () => {
+    return (
+        <Navbar expand="lg" className="mb-4 navbar-custom fixed-top">
+            <Navbar.Brand href="/" className="brand-text">
+                <strong> <i id='brand'>quiZ</i></strong>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className='toggleBar'/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/" className="nav-link">Home</Nav.Link>
+                    <Nav.Link href="/general" className="nav-link">General Knowledge</Nav.Link>
+                    <Nav.Link href="/sport" className="nav-link">Sport</Nav.Link>
+                    <Nav.Link href="/nature" className="nav-link">Nature</Nav.Link>
+                    <Nav.Link href="/historical" className="nav-link">Historical</Nav.Link>
+                    <Nav.Link href="/funFact" className="nav-link">Fun Facts</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default MyNavbar;
